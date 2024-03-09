2f35e00f4ece6a1094150d9892f589f86703bb35
AndreiCIlies <116495791+AndreiCIlies@users.noreply.github.com>
2024-03-09 13:12:27 +0200

    added users in admin.txt file and Login button logic

--- Tema 1/AdminMode.xaml.cs
+++ Tema 1/AdminMode.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,35 @@ namespace Tema_1
         public AdminMode()
         {
             InitializeComponent();
+        }
+
+        private void loginModeBtn(object sender, RoutedEventArgs e)
+        {
+            string[] users = File.ReadAllLines("C://Users//usER//Desktop//Anul_II//Semestrul_II//MAP//Teme//Tema 1//Tema 1//Tema 1//admin.txt");
+
+            string enteredUsername = username.Text;
+            string enteredPassword = password.Password;
+            bool isAdmin = false;
+
+            foreach(string user in users)
+            {
+                string[] usernameAndPassword = user.Split(' ');
+                if (usernameAndPassword.Length == 2 && usernameAndPassword[0] == enteredUsername && usernameAndPassword[1] == enteredPassword)
+                {
+                    isAdmin = true;
+                    break;
+                }
+            }
+
+            if(isAdmin)
+            {
+                AdminModeEditor adminModeEditorWindow = new AdminModeEditor();
+                adminModeEditorWindow.Show();
+            }
+            else
+            {
+                MessageBox.Show("Admin not found", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
