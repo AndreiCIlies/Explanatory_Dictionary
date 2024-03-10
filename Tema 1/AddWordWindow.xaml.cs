02bffe72d4feee5544f466657bb325c6ab020a93
AndreiCIlies <116495791+AndreiCIlies@users.noreply.github.com>
2024-03-10 11:38:12 +0200

    added AddWordWindow, EditWordWindow and DeleteWordWindow

--- Tema 1/AddWordWindow.xaml.cs
+++ Tema 1/AddWordWindow.xaml.cs
@@ -15,29 +15,18 @@ using System.Windows.Shapes;
 namespace Tema_1
 {
     /// <summary>
-    /// Interaction logic for AdminModeEditor.xaml
+    /// Interaction logic for AddWordWindow.xaml
     /// </summary>
-    public partial class AdminModeEditor : Window
+    public partial class AddWordWindow : Window
     {
-        public AdminModeEditor()
+        public AddWordWindow()
         {
             InitializeComponent();
         }
 
-        private void addWordBtn(object sender, RoutedEventArgs e)
+        private void addWordInDictionaryBtn(object sender, RoutedEventArgs e)
         {
-            AddWordWindow addWordWindow = new AddWordWindow();
-            addWordWindow.Show();
-        }
-
-        private void editWordBtn(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void deleteWordBtn(object sender, RoutedEventArgs e)
-        {
-
+            
         }
     }
 }
---
+++ Tema 1/EditWordWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Tema_1
+{
+    /// <summary>
+    /// Interaction logic for EditWordWindow.xaml
+    /// </summary>
+    public partial class EditWordWindow : Window
+    {
+        public EditWordWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
