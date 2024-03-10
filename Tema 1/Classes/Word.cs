43f3f384b33d59f4c668356a304735615b8f8193
AndreiCIlies <116495791+AndreiCIlies@users.noreply.github.com>
2024-03-10 14:57:36 +0200

    added class Word

---
+++ Tema 1/Classes/Word.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Tema_1.Classes
+{
+    internal class Word
+    {
+        public String word { get; set; }
+        public String description { get; set; }
+        public Image image { get; set; }
+        public String category { get; set; }
+
+        public Word(String word, String description, Image image, String category)
+        {
+            this.word = word;
+            this.description = description;
+            this.image = image;
+            this.category = category;
+        }
+
+        public Word(String word, String description, String category)
+        {
+            this.word = word;
+            this.description = description;
+            this.category = category;
+        }
+    }
+}
