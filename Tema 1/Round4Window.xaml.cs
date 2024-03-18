9ac6a6a1af27423ae11ed78d38499ec0075a6de3
AndreiCIlies <116495791+AndreiCIlies@users.noreply.github.com>
2024-03-18 23:08:30 +0200

    added Windows for every round and made round 5 window to end the game

--- Tema 1/Round4Window.xaml.cs
+++ Tema 1/Round4Window.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,82 @@ namespace Tema_1
     /// </summary>
     public partial class Round4Window : Window
     {
+        private string[] words;
+        private string[] descriptions;
+        private string[] images;
+
+        private string selectedWord;
+        private string selectedDescription;
+        private string selectedImage;
+
         public Round4Window()
         {
             InitializeComponent();
+            LoadWords();
+            SelectRandomWord();
+            DisplayWordInfo();
+        }
+
+        private void LoadWords()
+        {
+            string filePath = "C://Users//usER//Desktop//Anul_II//Semestrul_II//MAP//Teme//Tema 1//Tema 1//Tema 1//words.txt";
+            string[] lines = File.ReadAllLines(filePath);
+            int count = lines.Length / 4;
+
+            words = new string[count];
+            descriptions = new string[count];
+            images = new string[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                words[i] = lines[i * 4];
+                descriptions[i] = lines[i * 4 + 1];
+                images[i] = lines[i * 4 + 2];
+            }
+        }
+
+        private void SelectRandomWord()
+        {
+            Random rand = new Random();
+            int index = rand.Next(words.Length);
+            selectedWord = words[index];
+            selectedDescription = descriptions[index];
+            selectedImage = images[index];
+        }
+
+        private void DisplayWordInfo()
+        {
+            descriptionTextBlock.Text = selectedDescription;
+
+            if (selectedImage != "unavailableImage.jpg")
+            {
+                imageTextBlock.Background = new ImageBrush(new BitmapImage(new Uri("C://Users//usER//Desktop//Anul_II//Semestrul_II//MAP//Teme//Tema 1//Tema 1//Tema 1//Images//" + selectedImage)));
+                descriptionTextBlock.Visibility = Visibility.Collapsed;
+                imageTextBlock.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                imageTextBlock.Background = null;
+                imageTextBlock.Visibility = Visibility.Collapsed;
+                descriptionTextBlock.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void checkAnswerBtn(object sender, RoutedEventArgs e)
+        {
+            string userAnswer = answer.Text.Trim();
+
+            if (userAnswer.ToLower() == selectedWord.ToLower())
+            {
+                Round5Window round5Window = new Round5Window();
+                round5Window.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Incorrect answer. Please try again.", "Incorrect", MessageBoxButton.OK, MessageBoxImage.Warning);
+                answer.Clear();
+            }
         }
     }
 }
